9d7f5527f2a399f59f2a986c90edb8c5a8d9a148
Joona Heinikari <joona.heinikari@elisanet.fi>
2019-01-22 11:16:00 +0200

    Added Utilities folder, PasswordHash class and a method to hash passwords + removed ValuesController.cs

---
+++ BackendToolsWebApi/BackendToolsWebApi/Utilities/PasswordHash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace BackendToolsWebApi.Utilities
+{
+    public class PasswordHash
+    {
+        // hash password with salt and returns it as string
+        public static string HashPassword(string password, string salt)
+        {
+            byte[] saltBytes = Encoding.ASCII.GetBytes(salt);
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8
+            ));
+
+            return hashed;
+        }
+    }
+}
